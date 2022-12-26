5e0fe0be173c749e9c8523cd09cb3410255e80f1
Becky Nasty <99774559+aurelius250@users.noreply.github.com>
2022-12-26 15:16:27 -0800

    Add basic game mechanics of rps for a round

--- rps.js
+++ rps.js
@@ -1,24 +1,21 @@
-console.log("Bananas");
-let continueProgram = true;
-
-
 function getComputerChoice() { 
     let computeRPS = Math.floor(Math.random() * 3); 
     let computedRPS;
     console.log(computeRPS) 
     switch(computeRPS) { 
         case 0:
-        computedRPS = "rock";
+        computedRPS = "rock"
         break; 
 
         case 1:
-        computedRPS = "paper"; 
+        computedRPS = "paper"
         break;
         
         case 2: 
-        computedRPS = "scissors";
+        computedRPS = "scissors"
         break;
     }
+    console.log(computedRPS);
     return computedRPS;
 }
 
@@ -26,25 +23,71 @@ function getComputerChoice() {
 function getUserChoice() { 
     let computeUserRPS = prompt("Enter rock paper or scissors."); 
     console.log(computeUserRPS);
+    return computeUserRPS;
 }
 
 function startGameRPS() { 
     let computerChoice = getComputerChoice(); 
     let userChoice = getUserChoice(); 
-    console.log(rpsMatchups); 
-    
-    
+    console.log(rpsMatchups(computerChoice, userChoice)); 
 }
 
 function rpsMatchups(computerChoice, userChoice) { 
-    if(computerChoice == "rock"){ 
+
+    if(computerChoice === "rock"){ 
         switch(userChoice){ 
-            case "rock": return "draw";
+            case "rock": return ("Draw! " + computerChoice + " draws with " + userChoice);
             break; 
-            case "paper": return "you win!"; 
+
+            case "scissors": return ("You win! " + userChoice + " beats " + computerChoice);
             break; 
-            case "scissors": return "you lose!";
-            break; 
+            
+            case "paper": return ("You lose! " + computerChoice + " beats " + userChoice);
+            break;
+            default: 
+            console.log("Incorrect input!");
         }
     }
+
+    if(computerChoice === "paper"){ 
+        switch(userChoice){ 
+            case "rock": return ("You win! " + userChoice + " beats " + computerChoice);
+            break; 
+
+            case "paper": return ("Draw! " + computerChoice + " draws with " + userChoice);
+            break; 
+            
+            case "scissors": return ("You lose! " + computerChoice + " beats " + userChoice);
+            break;
+            default: 
+            console.log("Incorrect input!");
+        }
+    }
+
+    if(computerChoice === "scissors"){ 
+        switch(userChoice){ 
+            case "rock": return ("You win! " + userChoice + " beats " + computerChoice);
+            break; 
+
+            case "paper": return ("You lose! " + computerChoice + " beats " + userChoice);
+            
+            case "scissors": return ("Draw! " + computerChoice + " draws with " + userChoice);
+
+            break;
+            default: 
+            console.log("Incorrect input!");
+        }
+    }
+    
 }
+
+// function gameEnd(computerChoice, userChoice, result){
+//     if((result === "draw"){
+//         console.log("Draw! " + computerChoice + " ties with " + userChoice);
+//     } else if(result === "win")
+//     }
+//         console.log(`$result! `) 
+
+// }
+
+startGameRPS(); 
